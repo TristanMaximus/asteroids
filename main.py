6c873b7e122882dcd8272212f97a1e7f3486e68e
TristanMaximus <tristan.minsk@gmail.com>
2025-07-26 21:39:04 +0200

    [CH2-L3] Asteroids. FPS
    Added clock to limit FPS to MAX_FPS of 60

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ os.environ['SDL_AUDIODRIVER'] = 'pulse'
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(MAX_FPS) / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")

20b243a46202eb3e49b5a184a5dc67791386780c
TristanMaximus <tristan.minsk@gmail.com>
2025-07-26 22:26:22 +0200

    [CH2-L6] Asteroids. Player. Rotate
    Added update and rotate methods to the Player class.
    Rotation logic on A and D.

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0.0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+    
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(MAX_FPS) / 1000
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":

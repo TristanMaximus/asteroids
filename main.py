76cdf7b12dbccba29b6bd685d95e5a6e934ad69a
TristanMaximus <tristan.minsk@gmail.com>
2025-07-27 22:19:20 +0200

    [CH3-L4] Asteroids. Shooting
    Added shots. shots group and shooting mechanics on
    SPACE key press

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 # needed to remove annoing ALSA sound driver errors. Pulse is the sound server of choice, I guess
 # sound driver/server can be confirmed by running >echo $PULSE_SERVER< (shoud see output if it's pulse)
@@ -16,9 +17,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
 
